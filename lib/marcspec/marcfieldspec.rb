ac77bb0b050b38545d2a54583ac569e5f1648479
BillDueber <bill@dueber.com>
2010-08-05 22:00:38 -0400

    Adding docs

--- lib/marcspec/marcfieldspec.rb
+++ lib/marcspec/marcfieldspec.rb
@@ -97,6 +97,14 @@ module MARCSpec
   # using the string 'LDR' to identify itself
   
   class LeaderSpec < ControlFieldSpec
+    
+    # Built to be syntax-compatible with ControlFieldSpec, the tag must always
+    # be 'LDR' (case matters)
+    #
+    # @param ['LDR'] tag The 'tag'; in this case, always 'LDR'
+    # @param [Fixnum, Range<Fixnum>] range substring specification (either one character or a range) to return
+    # instead of the whole leader.
+    
     def initialize (tag, range=nil)
       unless tag == 'LDR'
         raise ArgumentError "Tag must be 'LDR'"
@@ -105,6 +113,11 @@ module MARCSpec
       self.range = range
     end
     
+    # Return the appropriate value (either the leader or a subset of it) from the
+    # given record
+    #
+    # @param [MARC4J4R::Record] r A MARC4J4R Record
+    # @return [String] the leader or substring of the leader
     def marc_values r
       if @range
         return r.leader[@range]
