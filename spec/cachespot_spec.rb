9be1f18d14cde1af8e5e95393fc209b3c6bb3bd6
Bill Dueber <bill@dueber.com>
2010-09-04 22:59:41 -0400

    Added spec for cachespot

---
+++ spec/cachespot_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+# LEADER 00734njm a2200217uu 4500
+# 001    afc99990058366
+# 003    DLC
+# 005    20071104155141.9
+# 007    sd ummunniauub
+# 008    071103s1939    xxufmnne||||||||| u eng||
+# 010    $a afc99990058366
+# 040    $a DLC $c DLC
+# 245 04 $a The Texas ranger $h [sound recording] / $c Sung by Beale D. Taylor.
+# 260    $a Medina, Texas, $c 1939.
+# 300    $a 1 sound disc : $b analog, 33 1/3 rpm, mono. ; $c 12 in.
+# 651  0 $a Medina $z Texas $z United States of America.
+# 700 1  $a Lomax, John Avery, 1867-1948 $e Recording engineer.
+# 700 1  $a Lomax, Ruby T. (Ruby Terrill) $e Recording engineer.
+# 700 1  $a Taylor, Beale D. $e Singer.
+# 852    $a American Folklife Center, Library of Congress
+# 852    $a DLC
+
+describe "Cachespot" do
+  before do
+    @one = MARC4J4R::Reader.new("#{DIR}/data/one.dat").first
+  end
+  
+  it "should set a value and get it back" do
+    @one.cachespot['one'] = 1
+    @one.cachespot['one'].should.equal 1
+  end
+end
